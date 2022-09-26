4ac9b53da11dbb3bba595ac8c2f89ba529d40301
unknown <dinhxuanloc741997@gmail.com>
2022-09-26 20:09:29 +0700

    thuchanh

---
+++ src/TH1.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class TH1 {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("Bay gio la:" +date);
+    }
+}
